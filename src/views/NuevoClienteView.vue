81e9d480eee49b9c554fe3cb10e48216af9a6415
Arturo Zepeda <aaze92@gmail.com>
2023-09-07 23:55:09 -0600

    añado Formkit y empizo a utilizarlo en NuevoCliente

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { FormKit } from '@formkit/vue';
 import RouterLink from '../components/UI/RouterLink.vue';
 import Heading from '../components/UI/Heading.vue';
 defineProps({
@@ -14,5 +15,31 @@ defineProps({
             <RouterLink to="inicio">Regresar</RouterLink>
         </div>
         <Heading>{{ titulo }}</Heading>    
+        <div class="mx-auto mt-10 bg-white shadow">
+            <FormKit type="form" action="false">
+                <div class="p-8">
+                    <div class="grid grid-cols-1 gap-6">
+                        <div>
+                            <FormKit type="text" name="nombre" label="Nombre" placeholder="Nombre cliente" validation="required" :validation-messages="{ required: 'El nombre del cliente es obligatorio'}"/>
+                        </div>
+                        <div>
+                            <FormKit type="text" name="apellido" label="Apellido" placeholder="Apellido cliente" validation="required"/>
+                        </div>
+                        <div>
+                            <FormKit type="text" name="empresa" label="Empresa" placeholder="Empresa" validation="required"/>
+                        </div>
+                        <div>
+                            <FormKit type="text" name="email" label="Email" placeholder="Email" validation="required"/>
+                        </div>
+                        <div>
+                            <FormKit type="text" name="telefono" label="Teléfono" placeholder="No. 1234567890" validation="required"/>
+                        </div>
+                    </div>
+                    <div class="flex justify-end mt-6">
+                        <FormKit type="button" variant="primary">Agregar Cliente</FormKit>
+                    </div>
+                </div>
+            </FormKit>
+        </div>
     </div>
 </template>
