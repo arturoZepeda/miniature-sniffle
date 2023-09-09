0fca82e18ff948c2d4f6cf0453c477b2c619afc4
dnk92 <aaze92@gmail.com.com>
2023-09-09 00:40:12 -0500

    añado formsubmit

--- src/views/NuevoClienteView.vue
+++ src/views/NuevoClienteView.vue
@@ -1,4 +1,5 @@
 <script setup>
+import { reactive } from 'vue'
 import { FormKit } from '@formkit/vue';
 import RouterLink from '../components/UI/RouterLink.vue';
 import Heading from '../components/UI/Heading.vue';
@@ -8,6 +9,12 @@ defineProps({
         required: true
     }
 });
+
+
+
+const handleSubmit = (data) => {
+    console.log("data", data);
+}
 </script>
 <template>
     <div>
@@ -17,17 +24,17 @@ defineProps({
         <Heading>{{ titulo }}</Heading>
         <div class="mx-auto mt-10 bg-white shadow">
             <div class="mx-auto md:w2/3 py-20 px-6">
-                <FormKit type="form" action="false">
+                <FormKit type="form" submit-label="Guardar cliente" incomplete-message="No se han completado los datos mandatorios" @submit="handleSubmit">
                         <FormKit type="text" name="nombre" label="Nombre" placeholder="Nombre cliente" validation="required"
-                            :validation-messages="{ required: 'El nombre del cliente es obligatorio' }" />
+                            :validation-messages="{ required: 'El nombre del cliente es obligatorio' }"  />
                         <FormKit type="text" name="apellido" label="Apellido" placeholder="Apellido cliente" validation="required"
-                            :validation-messages="{ required: 'El Apellido del cliente es obligatorio' }"/>
-                        <FormKit type="text" name="empresa" label="Empresa" placeholder="Empresa" validation="required" />
+                            :validation-messages="{ required: 'El Apellido del cliente es obligatorio' }" />
                         <FormKit type="email" name="email" label="Email" placeholder="Email" validation="required" 
-                            :validation-messages="{ required: 'El Email del cliente es obligatorio' }"/>
+                            :validation-messages="{ required: 'El Email del cliente es obligatorio' }" />
                         <FormKit type="tel" name="telefono" label="Teléfono" placeholder="No. 1234567890" validation="required|matches:/^[0-9]{10}$/ " 
-                            :validation-messages="{ required: 'El teléfono del cliente es obligatorio', matches:'el formato de numero debe de ser a 10 digitos.' }"/>
-
+                            :validation-messages="{ required: 'El teléfono del cliente es obligatorio', matches:'El formato de numero debe de ser a 10 digitos.' }" />
+                        <FormKit type="text" name="empresa" label="Empresa" placeholder="Empresa"  />
+                        <FormKit type="text" name="puesto" label="Puesto" placeholder="Puesto"  />
                 </FormKit>
             </div>
         </div>
