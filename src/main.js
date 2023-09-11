fe7faa0dcb7f0f60d9af5f2de791489785f2859c
Arturo Zepeda <aaze92@gmail.com>
2023-09-10 21:39:17 -0600

    test

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './assets/main.css'
-import config from '../formkit.confg'
 import { plugin, defaultConfig } from '@formkit/vue'
 import { createApp } from 'vue'
 import App from './App.vue'
@@ -8,5 +7,5 @@ import router from './router'
 const app = createApp(App)
 
 app.use(router)
-app.use(plugin, defaultConfig(config))
+app.use(plugin, defaultConfig())
 app.mount('#app')
